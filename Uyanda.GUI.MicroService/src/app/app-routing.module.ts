cc952e438c20ab9ea1c07781888bb08dc3488029
uyandam <uyandamphunga@gmail.com>
2022-11-27 22:52:45 +0200

    add route to cart component

--- Uyanda.GUI.MicroService/src/app/app-routing.module.ts
+++ Uyanda.GUI.MicroService/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './pages/cart/cart.component';
 import { MenuComponent } from './pages/menu/menu.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/menu', pathMatch: 'full' },
-  { path: 'menu', component: MenuComponent}
+  { path: 'menu', component: MenuComponent},
+  { path: 'cart', component: CartComponent}
 ];
 
 @NgModule({
