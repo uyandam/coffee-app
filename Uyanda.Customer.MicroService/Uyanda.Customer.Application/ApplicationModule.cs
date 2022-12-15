dea46480cc6127e8e1e8c79e75b7a706ab72c2b3
uyandam <uyandamphunga@gmail.com>
2022-12-15 22:38:11 +0200

    create customer models

--- Uyanda.Customer.MicroService/Uyanda.Customer.Application/ApplicationModule.cs
+++ Uyanda.Customer.MicroService/Uyanda.Customer.Application/ApplicationModule.cs
@@ -1,7 +1,15 @@
-﻿namespace Uyanda.Customer.Application
-{
-    public class ApplicationModule
-    {
+﻿using Microsoft.Extensions.DependencyInjection;
+using Uyanda.Customer.Application.Features.CustomerManagement.Services;
 
+namespace Uyanda.Customer.Application
+{
+    public static class ApplicationModule
+    {
+        public static IServiceCollection AddApplicationModule(this IServiceCollection services)
+        {
+            services.AddTransient<ICustomerManagementService, CustomerManagementService>();
+
+            return services;
+        }
     }
 }
---
+++ Uyanda.Customer.MicroService/Uyanda.Customer.Persistence/Entities/CustomerEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uyanda.Customer.Application.Features.CustomerManagement.Models;
+
+namespace Uyanda.Customer.Persistence.Entities
+{
+    public class CustomerEntity
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Surname { get; set; }
+
+        public int Points { get; set; }
+
+        public ContactDetailsEntity? ContactDetails { get; set; }
+    }
+}
