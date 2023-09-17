9027acc7b6ab16373e615f72a96eb7ac9128dadc
uyandam <uyandamphunga@gmail.com>
2023-09-17 21:57:56 +0200

    test api call post method to get menu

--- Uyanda.GUI.React.MicroService/react-app/src/pages/Home.tsx
+++ Uyanda.GUI.React.MicroService/react-app/src/pages/Home.tsx
@@ -1,4 +1,16 @@
+
+import getMenu from "../services/beverage.service";
+
 function Home() {
+
+    console.log(getMenu().then(value => {
+        console.log('resolved', value);
+      }));
+
+      console.log(getMenu().catch(error => {
+        console.log('rejected', error);
+      }));
+
     return (<>
         <h1>Home page</h1>
     </>)
